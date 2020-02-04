87a95e43ee5688189c42afe34df1eb416b039914
Benjamin Feng <benjamin.feng@glassdoor.com>
2020-02-04 15:38:05 -0600

    Explicit no inline + extra tests

--- compare.zig
+++ compare.zig
@@ -36,6 +36,54 @@ pub fn orderUsize(comptime T: type, lhs: []const T, rhs: []const T) std.math.Ord
     return std.math.order(lhs.len, rhs.len);
 }
 
+pub fn orderUsizeSafetyTrue(comptime T: type, lhs: []const T, rhs: []const T) std.math.Order {
+    @setRuntimeSafety(true);
+    const usize_bytes = @sizeOf(usize);
+    const n = std.math.min(lhs.len, rhs.len);
+    var i: usize = 0;
+    while ((i + 1) * usize_bytes <= n) : (i += usize_bytes) {
+        const a = std.mem.readIntBig(usize, @ptrCast(*const [usize_bytes]u8, &lhs[i]));
+        const b = std.mem.readIntBig(usize, @ptrCast(*const [usize_bytes]u8, &rhs[i]));
+        switch (std.math.order(a, b)) {
+            .eq => continue,
+            .lt => return .lt,
+            .gt => return .gt,
+        }
+    }
+    while (i < n) : (i += 1) {
+        switch (std.math.order(lhs[i], rhs[i])) {
+            .eq => continue,
+            .lt => return .lt,
+            .gt => return .gt,
+        }
+    }
+    return std.math.order(lhs.len, rhs.len);
+}
+
+pub fn orderUsizeSafetyFalse(comptime T: type, lhs: []const T, rhs: []const T) std.math.Order {
+    @setRuntimeSafety(false);
+    const usize_bytes = @sizeOf(usize);
+    const n = std.math.min(lhs.len, rhs.len);
+    var i: usize = 0;
+    while ((i + 1) * usize_bytes <= n) : (i += usize_bytes) {
+        const a = std.mem.readIntBig(usize, @ptrCast(*const [usize_bytes]u8, &lhs[i]));
+        const b = std.mem.readIntBig(usize, @ptrCast(*const [usize_bytes]u8, &rhs[i]));
+        switch (std.math.order(a, b)) {
+            .eq => continue,
+            .lt => return .lt,
+            .gt => return .gt,
+        }
+    }
+    while (i < n) : (i += 1) {
+        switch (std.math.order(lhs[i], rhs[i])) {
+            .eq => continue,
+            .lt => return .lt,
+            .gt => return .gt,
+        }
+    }
+    return std.math.order(lhs.len, rhs.len);
+}
+
 const bench = @import("bench.zig");
 test "benchmark" {
     try bench.benchmark(struct {
@@ -44,7 +92,7 @@ test "benchmark" {
             rhs: []const u8,
 
             fn bench(a: Arg, func: var) void {
-                _ = func(u8, a.lhs, a.rhs);
+                _ = @call(.{ .modifier = .never_inline }, func, .{ u8, a.lhs, a.rhs });
             }
         };
 
@@ -63,15 +111,23 @@ test "benchmark" {
         pub const iterations = 100000;
 
         pub fn @"-----"(a: Arg) void {
-            a.bench(order);
-        }
-
-        pub fn base(a: Arg) void {
-            a.bench(order);
-        }
-
-        pub fn improved(a: Arg) void {
             a.bench(orderUsize);
+        }
+
+        pub fn master(a: Arg) void {
+            a.bench(order);
+        }
+
+        pub fn Usize(a: Arg) void {
+            a.bench(orderUsize);
+        }
+
+        pub fn UsizeSafetyTrue(a: Arg) void {
+            a.bench(orderUsizeSafetyTrue);
+        }
+
+        pub fn UsizeSafetyFalse(a: Arg) void {
+            a.bench(orderUsizeSafetyFalse);
         }
     });
 }
